245911c9b7d32531fffc5783b3233f9f8dc577c9
Kelsey Yuen <heavenslashinghell@gmail.com>
2017-12-20 10:02:04 -0500

    Done

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,22 @@
 package quiz;
 
 public class ReverseString {
-    public String reverse(String input) {
+    /*public String reverse(String input) {
         return new StringBuffer(input).reverse().toString();
-    }
+    }*/
+	
+	public String reverse(String input) {
+		char[] tokens = new char[input.length()];
+		input.getChars(0, input.length(), tokens, 0);
+		
+		return recurse(tokens, 0);
+	}
+	
+	public String recurse(char[] tokens, int index) {
+		if (index < tokens.length) {
+			return recurse(tokens, index+1) + tokens[index];
+		} else {
+			return "";
+		}
+	}
 }
